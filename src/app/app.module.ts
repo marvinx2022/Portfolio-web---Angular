fdc157abc2fbb3d405ccac024c63aeffaaa59b4b
marvinx2022 <109774242+marvinx2022@users.noreply.github.com>
2022-11-14 23:08:16 -0300

    Header changed with html data on component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { StudiesComponent } from '../studies/studies.component';
+import { WorkComponent } from '../work/work.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule],
+  declarations: [ AppComponent, FooterComponent, HeaderComponent, ProjectsComponent, SkillsComponent, StudiesComponent, WorkComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
